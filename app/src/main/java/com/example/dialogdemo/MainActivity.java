42387c25090a52b88c1a295aa834bc38b6776b28
unknown <752094571@qq.com>
2017-02-26 19:57:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dialogdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dialogdemo;
+
+
+
+        import android.app.Application;
+
+        import android.test.ApplicationTestCase;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">
+ Testing Fundamentals</a>
+
+ */
+class ApplicationTest extends ApplicationTestCase<Application>
+{
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
